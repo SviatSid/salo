686a6fb5678842bb9a6df111293197d410fd390f
Sviatoslav Sidletskyi <ssid@softserveinc.com>
2017-05-08 12:31:43 +0300

    Add static directory and static resource

--- tram_api/resources/ui_resource.py
+++ tram_api/resources/ui_resource.py
@@ -1,9 +1,8 @@
-# from flask import send_from_directory
+from flask import send_from_directory
 from tram_api.resources import BaseResource
 
 
 class UIResource(BaseResource):
 
     def get(self):
-        # return current_app.send_static_file('index.html')
-        return {'status': 'ok'}
+        return send_from_directory('../static', 'index.html')
