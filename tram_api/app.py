7b55c32a64d1b3274a2828b7584895cc2073a54d
Sviatoslav Sidletskyi <ssid@softserveinc.com>
2017-05-08 15:56:18 +0300

    Static config

--- tram_api/app.py
+++ tram_api/app.py
@@ -9,7 +9,7 @@ from tram_api.resources.ui_resource import UIResource
 def create_app():
     app = Flask(__name__)
     api_bp = Blueprint('vk_api', __name__)
-    ui_bp = Blueprint('vk_ui', __name__, static_folder='static')
+    ui_bp = Blueprint('vk_ui', __name__, static_folder='../static', static_url_path='')
     ui_route = Api(ui_bp)
     api = swagger.docs(Api(api_bp), apiVersion='0.1', description='TramAPI')
 
